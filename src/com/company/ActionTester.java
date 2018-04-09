aad694c6215ca08534e54f9d53b4b56dc452c131
siham-sharif <siham.sharif@gmail.com>
2018-04-10 02:03:32 +0600

    Action Tester : Done; Didn't understand code

--- src/com/company/ActionTester.java
+++ src/com/company/ActionTester.java
@@ -2,6 +2,8 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 public class ActionTester {
@@ -21,9 +23,46 @@ public class ActionTester {
                 menu.add(new JMenuItem(printAction));
 
                 JToolBar toolBar = new JToolBar();
-                //toolBar.add(new);
+                toolBar.add(new JButton(printAction));
 
+                JButton enableButton = new JButton("Enable");
+                ActionListener enableActionListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        printAction.setEnabled(true);
+                    }
+                };
+                enableButton.addActionListener(enableActionListener);
 
+                JButton disableButton = new JButton("Disable");
+                ActionListener disableActionListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                              printAction.setEnabled(false);
+                    }
+                };
+                disableButton.addActionListener(disableActionListener);
+
+                JButton rebelButton = new JButton("Rebel");
+                ActionListener rebelActionListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        printAction.putValue(Action.NAME, "Hello World from rebel");
+                    }
+                };
+                rebelButton.addActionListener(rebelActionListener);
+
+                JPanel buttonPanel = new JPanel();
+                buttonPanel.add(enableButton);
+                buttonPanel.add(disableButton);
+                buttonPanel.add(rebelButton);
+
+                frame.setJMenuBar(menuBar);
+
+                frame.add(toolBar, BorderLayout.SOUTH);
+                frame.add(buttonPanel, BorderLayout.NORTH);
+                frame.setSize(300, 200);
+                frame.setVisible(true);
             }
         };
         EventQueue.invokeLater(runner);
